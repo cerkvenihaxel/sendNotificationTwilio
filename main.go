fa8d1de3ce296998f8ce73a21bd554446b1059e3
Axel Cerkvenih <cerkvenihaxel@gmail.com>
2024-12-20 11:03:29 -0300

    feature - general message endpoint

--- main.go
+++ main.go
@@ -90,11 +90,50 @@ func sendNotificationApproved(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Mensaje enviado correctamente"})
 }
 
+func generalMessage(c *gin.Context) {
+
+	var request struct {
+		PhoneNumber      string `json:"phone"`        // Número de teléfono
+		MessageVariables string `json:"message_vars"` // Variables del mensaje en formato JSON
+	}
+
+	if err := c.BindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Datos inválidos"})
+		return
+	}
+
+	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
+	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
+	fromNumber := os.Getenv("TWILIO_FROM_NUMBER")
+	contentSid := os.Getenv("TWILIO_CONTENT_SID_GENERAL")
+
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: accountSid,
+		Password: authToken,
+	})
+
+	params := &api.CreateMessageParams{}
+	params.SetTo("whatsapp:" + request.PhoneNumber)
+	params.SetFrom(fromNumber)
+	params.SetContentSid(contentSid)
+	params.SetContentVariables(request.MessageVariables)
+
+	_, err := client.Api.CreateMessage(params)
+	if err != nil {
+		log.Printf("Error al enviar el mensaje: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "No se pudo enviar el mensaje"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Mensaje enviado correctamente"})
+}
+
 func main() {
 	loadEnv()
 
 	r := gin.Default()
 	r.POST("/notification-medication", sendWhatsAppMessage)
 	r.POST("/notification-approved", sendNotificationApproved)
-	r.Run(":8080")
+	r.POST("/general-message", generalMessage)
+	r.Run(":8081")
 }
